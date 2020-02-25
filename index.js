b650bc877443222827ec90ca3d90f6fed2362f23
simba-mupasa <simbamupasa@yandex.com>
2020-02-25 08:30:01 -0800

    add db support

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-var db = require('./db');
+var session = require("express-session");
+var passport = require('./passport');
 
 app.use(express.static('public'));
+
+app.use(session({ secret: "my secret" }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-passport.use(new LocalStrategy(
-  async function(email, password, done) {
-    var result = await db.executeSelectQuery(`select email, password, name, role from uses where email='${email}'`);
-    console.log(result);
-    if (result.length === 0) {
-        return done(null, false, { message: 'Incorrect email.'});
-    }
-    const user = result[0];
-    if (user.password !== password) {
-        return done(null, false, { message: 'Incorrect password.'});
-    }
-    return done(null, user);
-  }
-));
 
 app.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/login.html'));
